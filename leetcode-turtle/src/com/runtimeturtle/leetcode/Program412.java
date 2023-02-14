6486cb6adfebcfc6e86bf9925a434339355543a9
white-parrot <everydaycodestory@gmail.com>
2023-02-14 15:38:01 +0530

    dsa-algo-turtle: This Commit is to Add New Solutions of LeetCode Problems

--- leetcode-turtle/src/com/runtimeturtle/leetcode/Program412.java
+++ leetcode-turtle/src/com/runtimeturtle/leetcode/Program412.java
@@ -8,19 +8,37 @@ public class Program412 {
     public List<String> fizzBuzz(int n) {
         String fizz = "Fizz";
         String buzz = "Buzz";
-        List<String> op = new ArrayList<>();
+        List<String> op1 = new ArrayList<>();
         for(int i = 1; i < n+1; i++){
             if(i % 15 == 0){
-                op.add(fizz + buzz);
+                op1.add(fizz + buzz);
             }else if(i % 3 == 0){
-                op.add(fizz);
+                op1.add(fizz);
             }else if(i % 5 == 0){
-                op.add(buzz);
+                op1.add(buzz);
             }else{
-                op.add(i + "");
+                op1.add(i + "");
             }
         }
-       return op;
+       
+       /* Other Solution*/
+       List<String> op2 = new ArrayList<>();
+       for(int i = 1; i <= n ; i++) {
+    	   String temp = "";
+    	   if(i % 3 == 0) {
+    		   temp = temp + fizz;
+    	   }
+    	   if(i % 5 == 0) {
+    		   temp = temp + buzz;
+    	   }
+    	   if(temp.isEmpty()) {
+    		   temp = String.valueOf(i);
+    	   }
+    	   op2.add(temp);
+       }
+
+       // return op1;       
+       return op2;
     }
 	
 }
