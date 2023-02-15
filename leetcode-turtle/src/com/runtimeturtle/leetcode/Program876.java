72e6aa711c9f3df26d5740651f376e791cc51da5
white-parrot <everydaycodestory@gmail.com>
2023-02-15 18:06:23 +0530

    This Commit is to Add New LeetCode Program

---
+++ leetcode-turtle/src/com/runtimeturtle/leetcode/Program876.java
@@ -0,0 +1,39 @@
+package com.runtimeturtle.leetcode;
+
+
+/* Definition for singly-linked list.*/
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+ public class Program876 {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode midNode = head;
+        ListNode next = head.next;
+        int count = 1;
+        while(next != null){
+            count++;
+            if(count % 2 == 0){
+                midNode = midNode.next;
+            }
+            next = next.next;
+        }
+        
+        /*Second Approach without Count*/
+        ListNode middle = head;
+        ListNode end = head;
+
+        while(end != null && end.next != null){
+            middle = middle.next;
+            end = end.next.next;
+        }
+
+        return middle;
+        // return midNode;
+    }
+
+}
