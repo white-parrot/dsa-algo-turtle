e4cf164c42c57ad3fc01badf58aa42a7acab624c
white-parrot <everydaycodestory@gmail.com>
2023-02-17 17:45:18 +0530

    dsa-algo-turtle: This Commit is to Update Practice Program

---
+++ algo&dsa-turtle/src/com/runtimeturtle/algo/arrayAndString/practice/Program01.java
@@ -0,0 +1,27 @@
+package com.runtimeturtle.algo.arrayAndString.practice;
+
+/* Given an array nums of integers, 
+ * return how many of them contain an even number of digits.
+ */
+public class Program01 {
+
+	/* Input: nums = [12,345,2,6,7896]
+	 * Output: 2
+	 * Explanation: 
+	 * 12 contains 2 digits (even number of digits). 
+	 * 345 contains 3 digits (odd number of digits). 
+	 *  2 contains 1 digit (odd number of digits). 
+	 *	6 contains 1 digit (odd number of digits). 
+	 *	7896 contains 4 digits (even number of digits). 
+	 * Therefore only 12 and 7896 contain an even number of digits.
+	 */
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for(int i : nums){
+            String temp = i + "";
+            count = temp.length() % 2 == 0 ? ++count : count;
+        }
+        return count;
+    }
+
+}
