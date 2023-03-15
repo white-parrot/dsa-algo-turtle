91834e23f6d7f1975ced152434f9906721ad070e
white-parrot <everydaycodestory@gmail.com>
2023-03-15 19:27:55 +0530

    dsa-algo-turtle: This Commit is to Add New Solutions

---
+++ leetcode-turtle/src/com/runtimeturtle/leetcode/Program3228.java
@@ -0,0 +1,43 @@
+package com.runtimeturtle.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * {@link}https://leetcode.com/explore/featured/card/fun-with-arrays/523/conclusion/3228/
+ * {@link}https://leetcode.com/problems/height-checker/
+ *
+ * @author white-parrot
+ */
+class Program3228 {
+    
+    public int heightChecker(int[] heights) {
+        
+        int[] expected = heights.clone();
+        Arrays.sort(heights);
+        
+        int track = 0;
+        
+        for(int i = 0; i < heights.length; i++){
+            if(expected[i] != heights[i]) track++;
+        }
+        // return track;
+        
+        /* Count Sort Approach */
+        // perform a bucket-sort
+        int[] bucket = new int[101];
+        for(int number : heights) {
+            bucket[number]++;
+        }
+        
+        // check the ammount of disparities between the input array and the bucket
+        int count = 0, index = 0;
+        for(int i = 1; i <= 100; i++) {
+            while(bucket[i] > 0) {
+                if(i != heights[index++]) count++;
+                bucket[i]--;
+            }
+        }
+        return count;
+    }
+
+}
