e60da17a5221662028b9a0786de32b800369d43b
black parrot <blackparrot@localhost.localdomain>
2023-03-19 23:32:49 +0530

    dsa-algo-turtle: This Commit is to Add New Solutions

---
+++ leetcode-turtle/src/com/runtimeturtle/leetcode/Program3270.java
@@ -0,0 +1,38 @@
+package com.runtimeturtle.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@link}https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3270/ 
+ * {@link}https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+ *
+ * @author white-parrot
+ */
+class Program3270 {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        
+        for(int i = 0; i < nums.length; i++){
+            int traceIndex = Math.abs(nums[i])-1;
+            int traceValue = nums[traceIndex];
+            if(!(traceValue < 0)){
+                nums[traceIndex] = traceValue * -1;
+            }            
+        }
+
+        List<Integer> result = new ArrayList<>();
+        
+        int count = 1;
+        for(int i : nums){
+            if(i > 0){
+                result.add(count);
+                
+            }
+            count++;
+        }
+        
+        return result;
+    }
+
+}
